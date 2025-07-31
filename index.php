22f87fb81d641956d398958be4e832fb6bd3c829
Adisak Yongpanya <664230034@webmail.npru.ac.th>
2025-07-31 11:15:03 +0700

    Refactor HTML structure and improve formatting in LoopProduct.php; add database connection and fetch data scripts; create product SQL table with initial data

--- index.php
+++ index.php
@@ -2,41 +2,41 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 
 <body>
-    <h1>Welcome to PHP Basic</h1>
-    <p>This is a simple PHP application</p>
-    <hr>
-    <h1 style="color:red">Basic PHP Syntax</h1>
-    <pre>
+  <h1>Welcome to PHP Basic</h1>
+  <p>This is a simple PHP application</p>
+  <hr>
+  <h1 style="color:red">Basic PHP Syntax</h1>
+  <pre>
   &lt;?php
     echo "Hello World";
     ?&gt;
   </pre>
-    <h3>Result</h3>
-    <div style="color: blue;">
-        <?php
-        echo "Hello World<br>";
-        print "<span style='color:lime'>Adisak Yongpanya</span>";
-        ?>
-    </div>
-    <hr>
-    <h1>MY MENU</h1>
-    <ul>
-        <li><a href="W01-01-introPHP.php">Intro PHP</a></li>
-        <li><a href="W02_01_oddEvenNumber.php">Odd-Even-Number</a></li>
-        <li><a href="W02_02_grade.php">Calculater Grade</a></li>
-        <li><a href="W06_01_LoopProduct.php">Show Product</a></li>
-        <li><a href="">Connect Database</a></li>
-        <li><a href="">Show Product (Form Database)</a></li>
-        
+  <h3>Result</h3>
+  <div style="color: blue;">
+    <?php
+    echo "Hello World<br>";
+    print "<span style='color:lime'>Adisak Yongpanya</span>";
+    ?>
+  </div>
+  <hr>
+  <h1>MY MENU</h1>
+  <ul>
+    <li><a href="W01-01-introPHP.php">Intro PHP</a></li>
+    <li><a href="W02_01_oddEvenNumber.php">Odd-Even-Number</a></li>
+    <li><a href="W02_02_grade.php">Calculater Grade</a></li>
+    <li><a href="W06_01_LoopProduct.php">Show Product</a></li>
+    <li><a href="W07_01_connectDB.php">Connect Database</a></li>
+    <li><a href="W07_02_fetchData.php">fetch data</a></li>
+    <li><a href="">Show Product (Form Database)</a></li>
 
 
-    </ul>
+  </ul>
 </body>
 
 </html>
